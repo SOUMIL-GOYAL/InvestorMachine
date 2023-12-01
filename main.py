7df54672beab65887d69e074abdc5ca565b843d6
SOUMIL-GOYAL <soumil.goyal@yahoo.com.sg>
2023-12-01 14:18:48 -0600

    fully working buying of stocks

--- main.py
+++ main.py
@@ -2,17 +2,10 @@ from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
 
-trading_client = TradingClient('PKD6PB46QLVPY3ABNUNB', 'WfvEzc8mN2EMokCLWN6yNkz0tRDyrxUvwtP0zT0V', paper=True)
+trading_client = TradingClient('PK342HRR8ZE8JCJNSMQL', 'vyqsQIbXBVppPgIz4X3vBMdoEGm0Y8j9cJunkY5W', paper=True)
 
 # preparing orders
-market_order_data = MarketOrderRequest(
-                    symbol="MSFT",
-                    qty=0.5,
-                    side=OrderSide.BUY,
-                    time_in_force=TimeInForce.DAY
-                    )
+market_order_data = MarketOrderRequest(symbol="MSFT", qty=1, side=OrderSide.BUY, time_in_force=TimeInForce.DAY)
 
 # Market order
-market_order = trading_client.submit_order(
-                order_data=market_order_data
-               )
+market_order = trading_client.submit_order(order_data=market_order_data)
