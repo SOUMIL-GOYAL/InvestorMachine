7a21aa375f62267c8b1ce6c4fe9522b7401bc5c2
SOUMIL-GOYAL <soumil.goyal@yahoo.com.sg>
2023-12-19 14:06:08 -0600

    no errors, makes money

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ from alpaca.data import StockHistoricalDataClient
 from alpaca.data.requests import StockLatestQuoteRequest
 import csv
 
+USE_API = True
+
 alpaca_secrets = {"Key": 'PKFX44Y3R3O86HJTBHB2', "Secret" : 'BaaU98K0PYhBNFPr9TiqckKHRvGdWwb4w42I3GDw'}
-openweather_secrets = {"Key": "7d99e344057c583ac695552ab47ebb07"}
+openweather_secrets = {"base_url" : "http://api.openweathermap.org/data/2.5/weather?", "Key": "7d99e344057c583ac695552ab47ebb07", "city_name" : "Singapore", }
 
 trading_client = TradingClient(alpaca_secrets["Key"], alpaca_secrets["Secret"], paper=True)
 stock_client = StockHistoricalDataClient(alpaca_secrets["Key"],  alpaca_secrets["Secret"])
 
 spy = {"symbol" : "SPY", "history" : [], "owned": False}
-investments = [spy, ]
+investments = [spy]
 
 counter = 0
 
@@ -30,6 +32,10 @@ counter = 0
 # print(trading_client.get_account())
 
 while (True):
+	if (counter > 50):
+		print("50 counts complete, breaking loop")
+		break
+
 	if (trading_client.get_clock().is_open == False): #market is over # type: ignore
 		print("market is not open; breaking loop")
 		break
@@ -42,6 +48,8 @@ while (True):
 	# 		write.writerow(x)
 
 	for stock in investments:
+		
+
 		multisymbol_request_params = StockLatestQuoteRequest(symbol_or_symbols= [stock["symbol"]])
 		latest_multisymbol_quotes = stock_client.get_stock_latest_quote(multisymbol_request_params)
 		spy["history"].append(latest_multisymbol_quotes[stock["symbol"]].ask_price)
@@ -54,12 +62,14 @@ while (True):
 			stock["owned"] = False
 			print("unowned")
 
+
 		if (stock["owned"] == False): #unowned
+
 			if (len(stock["history"]) > 3): 
 				money = float(trading_client.get_account().daytrading_buying_power) # type: ignore
 				print("$", money, " available")
 				if (stock["history"][-1] > stock["history"][-2] and stock["history"][-2] > stock["history"][-3]): #stock trade to be make
-					market_order_data = MarketOrderRequest(symbol="SPY", notional= money, side=OrderSide.BUY, time_in_force=TimeInForce.DAY)
+					market_order_data = MarketOrderRequest(symbol="SPY", notional= money/len(investments), side=OrderSide.BUY, time_in_force=TimeInForce.DAY)
 					market_order = trading_client.submit_order(order_data=market_order_data)
 					print("bought")
 					
@@ -74,11 +84,7 @@ while (True):
 					print("shorted")
 				else:
 					print("neither shorting not buying!")
-					
-				counter = counter + 1
-
-				if (counter > 50):
-					break
+					counter = counter + 1
 				
 		elif (stock["owned"] == True): #owned
 
@@ -88,13 +94,48 @@ while (True):
 			if (inprice < nowprice): #position is profitable # type: ignore
 				trading_client.close_position(stock["symbol"])
 				print("sold! we made $", (nowprice-inprice) * amountowned)
+				stock["history"] = []
+				sleep(2)
 			else:
 
 				print("not sold, holding...")
-	if (counter % 5):
-		sleep(10)
-	else:
-		sleep(1)
+
+	
+	if (USE_API):
+		#city_name = input("Enter city name : ")
+		complete_url = openweather_secrets["base_url"] + "appid=" + openweather_secrets["Key"] + "&q=" + openweather_secrets["city_name"]
+		response = requests.get(complete_url)
+		x = response.json()
+		# print(x)
+		# 404 means sity is not found
+		if x["cod"] != "404":
+			y = x["main"]
+			current_temperature = y["temp"]
+
+			# print(" Temperature (in kelvin unit) = " +
+			# 				str(current_temperature) +
+			# 	"\n atmospheric pressure (in hPa unit) = " +
+			# 				str(current_pressure) +
+			# 	"\n humidity (in percentage) = " +
+			# 				str(current_humidity) +
+			# 	"\n description = " +
+			# 				str(weather_description))
+			try:
+				trading_client.get_open_position("KO")
+				koowned = True
+			except:
+				koowned = False
+				# print("unowned")
+			if (float(current_temperature) >= 303.15 and koowned == False):
+				market_order_data = MarketOrderRequest(symbol="KO", notional= 1, side=OrderSide.BUY, time_in_force=TimeInForce.DAY)
+				market_order = trading_client.submit_order(order_data=market_order_data)
+			elif (koowned == True):
+				trading_client.close_position("KO")
+		else:
+			print(" City Not Found ")
+	
+
+	sleep(1)
 	
 
 
@@ -102,49 +143,4 @@ while (True):
 
 
 
-# base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-# city_name = "Singapore" #input("Enter city name : ")
-
-
-# complete_url = base_url + "appid=" + openweather_secrets["Key"] + "&q=" + city_name
-
-
-# response = requests.get(complete_url)
-
-
-# x = response.json()
-# # print(x)
-
-# # 404 means sity is not found
-# if x["cod"] != "404":
-
-    
-# 	y = x["main"]
-
-
-# 	current_temperature = y["temp"]
-
-
-# 	current_pressure = y["pressure"]
-
-
-# 	current_humidity = y["humidity"]
-
-
-# 	z = x["weather"]
-
-
-# 	weather_description = z[0]["description"]
-
-# 	# print(" Temperature (in kelvin unit) = " +
-# 	# 				str(current_temperature) +
-# 	# 	"\n atmospheric pressure (in hPa unit) = " +
-# 	# 				str(current_pressure) +
-# 	# 	"\n humidity (in percentage) = " +
-# 	# 				str(current_humidity) +
-# 	# 	"\n description = " +
-# 	# 				str(weather_description))
-
-# else:
-# 	print(" City Not Found ")
